24e34e09c2e3102139852e6b179c2e30fdb612f6
Alina Sidorova <alinasidorova2812@gmail.com>
2020-03-16 16:35:48 +0300

    docs: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length%2 !== 0){
+    return false;
+  }
+  let stack = [];
+  let config = [].concat(...bracketsConfig);
+  for(let el of str){
+    if(el === "7" || el === "8" || el === "|"){
+      if(stack[stack.length-1] === el){
+        stack.pop();
+      }
+      else{
+        stack.push(el);
+      }
+    }
+    else{
+      if(config.indexOf(el)%2 === 0){
+        stack.push(el);
+      }
+      else{
+        if(stack[stack.length-1] === config[config.indexOf(el)-1]){
+          stack.pop();
+        }
+      }
+    }
+  }
+  return stack.length === 0 ? true : false;
 }
